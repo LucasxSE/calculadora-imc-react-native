0e252db421528976a1fe475516ea8f9ac6dfdd8d
Lucas Maciel Monteiro <65786594+LucasxSE@users.noreply.github.com>
2022-06-06 08:57:26 -0400

    Adicionando pasta source
    Pasta source com as páginas de navegação do app, a Home e Calculator

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+function Home() {
+    return (
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+        <Text>Tela de Início</Text>
+      </View>
+    );
+}
