6541d07dd3c57df90212b04e07c463aeec24bfa6
Lucas Maciel Monteiro <65786594+LucasxSE@users.noreply.github.com>
2022-06-07 11:32:27 -0400

    Adição de novas Stacks e imports

--- App.js
+++ App.js
@@ -2,17 +2,30 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from './src/pages/Home';
-import { Calculator } from './src/pages/Calculator'
+import HomeScreen from './src/pages/Home';
+import Sobre from './src/pages/Sobre';
+import Calculadora from './src/pages/Calculadora';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Calculadora' component={Calculator}/>
-        <Stack.Screen name='Home' component={Home}/>
+      <Stack.Screen 
+          name='Home' 
+          component={HomeScreen} 
+          options={{
+            title: 'Início'
+          }}
+        />
+        <Stack.Screen 
+          name='Calculadora' 
+          component={Calculadora} 
+        />
+        <Stack.Screen 
+          name='Sobre' 
+          component={Sobre} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
