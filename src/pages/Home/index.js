fae0dc7efbb30d20db7eafe0f71179bfbc570cad
Lucas Maciel Monteiro <65786594+LucasxSE@users.noreply.github.com>
2022-06-07 11:34:13 -0400

    Tela Inicial
    Adição de imagens como botões de navegação e estilização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,41 @@
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-function Home() {
-    return (
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>Tela de Início</Text>
-      </View>
-    );
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Calculadora')}>
+        <Image
+          style={styles.imagem}
+          source={require('../../../assets/calculadora.png')}
+          />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Sobre')}>
+        <Image
+        style={styles.imagem}
+        source={require('../../../assets/sobre.png')}
+        />
+      </TouchableOpacity>
+      
+      
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+
+  imagem: {
+    width: 100,
+    height: 100,
+  },
+});
