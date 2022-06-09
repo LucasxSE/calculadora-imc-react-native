73302a88e10147c70af85ad3f69a2d54d3855a12
Lucas Maciel Monteiro <65786594+LucasxSE@users.noreply.github.com>
2022-06-09 11:26:51 -0400

    Bottom Tabs

--- App.js
+++ App.js
@@ -1,32 +1,55 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomeScreen from './src/pages/Home';
 import Sobre from './src/pages/Sobre';
 import Calculadora from './src/pages/Calculadora';
 
 const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return(
+    <Tab.Navigator shifting={true} barStyle={{ backgroundColor: '#00ab94' }} >
+      <Tab.Screen 
+      name='Home' 
+      component={HomeScreen} 
+      options={{
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name='home' size={24} color='white' />
+        ),
+      }}
+      />
+      <Tab.Screen 
+      name='Calculadora' 
+      component={Calculadora}
+      options={{
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name='calculator' size={24} color='white' />
+        ),
+      }} 
+      />
+      <Tab.Screen 
+      name='Sobre' 
+      component={Sobre}
+      options={{
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name='help' size={24} color='white' />
+        ),
+      }} 
+      />
+    </Tab.Navigator>
+  );
+  
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Screen 
-          name='Home' 
-          component={HomeScreen} 
-          options={{
-            title: 'Início'
-          }}
-        />
-        <Stack.Screen 
-          name='Calculadora' 
-          component={Calculadora} 
-        />
-        <Stack.Screen 
-          name='Sobre' 
-          component={Sobre} 
-        />
-      </Stack.Navigator>
+      <MyTabs/>
     </NavigationContainer>
   );
 }
